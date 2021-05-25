3e9ce0409abf49d9bfecd159b6cc4194009b1d83
Thomas Huchedé <thomas.huchede@zenika.com>
2021-05-25 18:59:08 +0200

    step 5: add server-side cache

--- server.ts
+++ server.ts
@@ -7,17 +7,25 @@ import { join } from 'path';
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
+import * as cache from 'memory-cache';
+
+const CACHE_DURATION_IN_MS = 60 * 60 * 1000; // 1 hour
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/grenoble-hands-on/browser');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? 'index.original.html'
+    : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine('html', ngExpressEngine({
-    bootstrap: AppServerModule,
-  }));
+  server.engine(
+    'html',
+    ngExpressEngine({
+      bootstrap: AppServerModule,
+    })
+  );
 
   server.set('view engine', 'html');
   server.set('views', distFolder);
@@ -25,13 +33,31 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+  server.get(
+    '*.*',
+    express.static(distFolder, {
+      maxAge: '1y',
+    })
+  );
 
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+    const cachedHtml = cache.get(req.originalUrl);
+    if (cachedHtml) {
+      res.send(cachedHtml);
+    } else {
+      res.render(
+        indexHtml,
+        {
+          req,
+          providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+        },
+        (_, html) => {
+          cache.put(req.originalUrl, html, CACHE_DURATION_IN_MS);
+          res.send(html);
+        }
+      );
+    }
   });
 
   return server;
@@ -52,7 +78,7 @@ function run(): void {
 // The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
-const moduleFilename = mainModule && mainModule.filename || '';
+const moduleFilename = (mainModule && mainModule.filename) || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
