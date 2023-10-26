f0485f3756d132ccdbac02ff749358ba93d20935
Roger Sirito-Olivier <roger.sirito-olivier@zenika.com>
2023-10-26 10:04:44 +0200

    resolve PR comment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import {routes} from './app/app.routing';
+import {routes} from './app/app.routes';
 import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import {provideRouter} from "@angular/router";
